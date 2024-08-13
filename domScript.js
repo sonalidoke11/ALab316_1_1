c723da53c2c52b78f8daa938e4e80ea7446ef6e7
sonalidoke11 <sonalidoke810@gmail.com>
2024-08-13 15:00:39 -0400

    First Commit

---
+++ domScript.js
@@ -0,0 +1,7 @@
+const mainEl = document.querySelector("main")
+console.log(mainEl)
+
+
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML="<h1>DOM Manipulation</h1>"
+mainEl.classList.add("flex-ctr");
